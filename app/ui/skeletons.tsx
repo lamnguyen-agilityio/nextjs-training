a15d8bc8033c4d6b1535f4436adc2b4584b943fc
Lam Nguyen <lam.nguyen@asnet.com.vn>
2024-01-08 14:33:23 +0700

    #9 Feature/ Implement feature streaming

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -24,7 +24,6 @@ export function CardsSkeleton() {
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
-      <CardSkeleton />
     </>
   );
 }
@@ -89,7 +88,6 @@ export default function DashboardSkeleton() {
         className={`${shimmer} relative mb-4 h-8 w-36 overflow-hidden rounded-md bg-gray-100`}
       />
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <CardSkeleton />
         <CardSkeleton />
         <CardSkeleton />
         <CardSkeleton />
