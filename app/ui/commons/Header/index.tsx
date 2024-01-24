810c287f1441cf0f81610a2dbc489596667bae3e
Lam Nguyen <lam.nguyen@asnet.com.vn>
2024-01-24 09:40:33 +0700

    #23 Feature/ Implement Search component

--- app/ui/commons/Header/index.tsx
+++ app/ui/commons/Header/index.tsx
@@ -1,27 +1,37 @@
+'use client';
+
 import Image from 'next/image';
 
 // Icons
-import { SearchIcon, NotificationIcon } from '@/app/ui/icons';
+import { NotificationIcon } from '@/app/ui/icons';
 
 // Images
 import avatar from '@/public/images/avatar.png';
+import Search from '../Search';
 
-const Header = () => (
-  <header className="flex justify-between items-center h-36 pt-10 pb-14 pr-10">
-    <h4 className="text-xl font-semibold text-fill-text-dark">My course</h4>
-    <div className="flex items-center gap-7">
-      <SearchIcon className="cursor-pointer" />
-      <NotificationIcon className="cursor-not-allowed" />
-      <Image
-        priority
-        src={avatar}
-        width={50}
-        height={50}
-        alt="avatar"
-        className="cursor-not-allowed"
-      />
-    </div>
-  </header>
-);
+const Header = () => {
+  // TODO: Handle search courses
+  const handleChange = (value: string) => {
+    console.log(value);
+  };
+
+  return (
+    <header className="flex justify-between items-center h-36 pt-10 pb-14 pr-10">
+      <h4 className="text-xl font-semibold text-fill-text-dark">My course</h4>
+      <div className="flex items-center gap-7">
+        <Search onChange={handleChange} />
+        <NotificationIcon className="cursor-not-allowed" />
+        <Image
+          priority
+          src={avatar}
+          width={50}
+          height={50}
+          alt="avatar"
+          className="cursor-not-allowed"
+        />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
